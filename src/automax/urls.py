2ef994e9da1dd21778814f944b0d05430921e413
arshadmaniyar <arshad.maniyar@esds.co.in>
2025-07-08 10:05:40 +0530

    login form added changes

--- src/automax/urls.py
+++ src/automax/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.urls import path,include
 from main import urls as main_urls
 from django.conf.urls.static import static
+from users import urls as users_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(main_urls))
+    path('', include(main_urls)),
+    path('', include(users_urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
 # Create your views here.
+def login_view(request):
+    login_form = AuthenticationForm()
+    
+    return render(request, "views/login.html", {"login_form": login_form})
